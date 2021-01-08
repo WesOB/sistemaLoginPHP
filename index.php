e6d4fb6e7d982bc5825192c4058e2b89b63b0e1e
WesOB <wesx.si@gmail.com>
2021-01-08 10:31:58 -0300

    add background on page

--- index.php
+++ index.php
@@ -6,13 +6,14 @@
     <link rel="stylesheet" href="CSS/estilo.css">
   </head>
   <body>
-    <form class="" action="processa.php" method="post">
-      <h1>Entrar</h1>
-      <input type="email" name="" value="" placeholder="Usuário">
-      <input type="password" name="" value="" placeholder="Senha">
-      <input type="submit" name="" value="ACESSAR">
-      <a href="#">Ainda não é inscrito <strong>Cadastre-se!</strong></a>
-    </form>
-
+    <div class="corpo-form">
+      <form class="" action="processa.php" method="post">
+        <h1>Entrar</h1>
+        <input type="email" name="" value="" placeholder="Usuário">
+        <input type="password" name="" value="" placeholder="Senha">
+        <input type="submit" name="" value="ACESSAR">
+        <a href="#">Ainda não é inscrito <strong>Cadastre-se!</strong></a>
+      </form>
+    </div>
   </body>
 </html>
