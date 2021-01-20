1f574f5a67da4ab14e98f3fd9e6e6610336c3a37
WesOB <wesx.si@gmail.com>
2021-01-20 15:48:22 -0300

    add page cadastrar

---
+++ cadastrar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Projeto Login</title>
+    <link rel="stylesheet" href="CSS/estilo.css">
+  </head>
+  <body>
+    <div id="corpo-form">
+      <form class="" action="processa.php" method="post">
+        <h1>Cadastrar</h1>
+        <input type="text" name="" value="" placeholder="Nome Completo">
+        <input type="text" name="" value="" placeholder="Telefone">
+        <input type="email" name="" value="" placeholder="Usuário">
+        <input type="password" name="" value="" placeholder="Senha">
+        <input type="password" name="" value="" placeholder="Confirmar senha">
+        <input type="submit" name="" value="CADASTRAR">
+      </form>
+    </div>
+  </body>
+</html>
