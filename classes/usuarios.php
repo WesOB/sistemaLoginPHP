de2ed761e86e82f96fd3b753ae713aed8676cbb2
WesOB <wesx.si@gmail.com>
2021-01-20 16:46:03 -0300

    add folder classes and add connection with db

---
+++ classes/usuarios.php
@@ -0,0 +1,32 @@
+<?php
+Class Usuario{
+  private $pdo;
+  public $msgErro = "";
+  public function conectar($nome, $host, $usuario, $senha){
+    global $pdo;
+    try {
+        $pdo = new PDO("mysql:dbname=".$nome.";host=".$host,$usuario,$senha);
+    } catch (PDOException $e) {
+      msgErro = $e->getMessage();
+
+    }catch (Exception $e) {
+      msgErro = $e->getMessage();
+   }
+
+  }
+  public function cadastrar(){
+    global $pdo;
+
+  }
+  public function logar(){
+      global $pdo;
+  }
+
+
+}
+
+
+
+
+
+ ?>
